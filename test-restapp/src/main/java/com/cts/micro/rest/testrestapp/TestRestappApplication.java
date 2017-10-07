e733869b3f730a4c7deb19a138d4eae466a77ed3
sadmin <sadmin@PC373390.cts.com>
2017-10-07 12:04:22 +0530

    Hi! I'm Uma and this is my first git push -UD

---
+++ test-restapp/src/main/java/com/cts/micro/rest/testrestapp/TestRestappApplication.java
@@ -0,0 +1,12 @@
+package com.cts.micro.rest.testrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestappApplication.class, args);
+	}
+}
